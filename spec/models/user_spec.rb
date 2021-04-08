066d6719f7a679c485646e0c3b4276c1fead74e8
pengin0805 <79044858+pengin0805@users.noreply.github.com>
2021-04-08 19:38:15 +0900

    テストコード user_spec.rbのテスト内容の洗い出し

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,29 +5,49 @@ RSpec.describe User, type: :model do
     @user = FactoryBot.build(:user)
   end
 
-  describe '新規登録/ユーザー情報' do
-    it 'nicknameが空では登録できない' do
+  describe '新規登録' do
+    context 'ユーザーの新規登録できないとき' do
+     it 'nicknameが空では登録できない' do
+     end
+     it 'passwordとpassword_confirmationが6文字以上であれば登録できる' do
+     end
+     it 'emailが空では登録できない' do
+     end
+     it '重複したemailが存在する場合登録できないこと' do
+     end
+     it 'emailに@が空と登録できない' do
+     end
+     it 'passwordが空では登録できない' do
+     end
+     it 'passwordが5文字以下では登録できない' do
+     end
+     it 'passwordは半角英数字が混合されていれば登録が可能なこと' do
+     end
+     it 'passwordが存在してもpassword_confirmationが空では登録できない' do
+     end
+     it 'passwordとpassword_confirmationが一致していないと登録できない' do
+     end
     end
-    it 'passwordとpassword_confirmationが6文字以上であれば登録できる' do
-    end
-    it 'emailが空では登録できない' do
-    end
-    it '重複したemailが存在する場合登録できないこと' do
-    end
-    it 'emailに@がないと登録できない' do
-    end
-    it 'passwordが空では登録できない' do
-    end
-    it 'passwordが5文字以下では登録できない' do
-    end
-    it 'passwordは半角英数字が混合されていれば、登録が可能なこと' do
-    end
-    
-    it 'passwordが存在してもpassword_confirmationが空では登録できない' do
-    end
-    it 'nicknameが7文字以上では登録できない' do
-    end
-    it '重複したemailが存在する場合登録できない' do
+
+    context 'ユーザーの新規登録できないとき' do
+      it 'last_nameが空と登録できない' do
+      end
+      it 'first_nameが空と登録できない' do
+      end
+      it 'last_nameは半角での入力だと登録できない' do
+      end
+      it 'first_nameは半角での入力だと登録できない' do
+      end
+      it 'last_name_kanaが空と登録できない' do
+      end
+      it 'first_name_kanaが空と登録できない' do
+      end
+      it 'last_name_kanaは半角での入力だと登録できない' do
+      end
+      it 'first_name_kanaは半角での入力だと登録できない' do
+      end
+      it 'birthdayが空と登録できない' do
+      end
     end
   end
 end
