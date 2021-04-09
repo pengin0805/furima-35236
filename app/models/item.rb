e94f2558d9e8864474e6f37705f10fc8e81957c4
pengin0805 <79044858+pengin0805@users.noreply.github.com>
2021-04-09 16:44:08 +0900

    アソシエーション及びバリデーション記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,13 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  
+  validates :item_name, presence: true
+  validates :item_info, presence: true
+  validates :category_id, presence: true
+  validates :condition_id, presence: true
+  validates :shipping_fee_id, presence: true
+  validates :shipping_area_id, presence: true
+  validates :shipping_day_id, presence: true
+  validates :price, presence: true
+  
 end
