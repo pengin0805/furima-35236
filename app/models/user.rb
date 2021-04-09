235cc98f6ff45ce1725517494a107a332642f379
pengin0805 <79044858+pengin0805@users.noreply.github.com>
2021-04-09 14:08:50 +0900

    正規表現の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)\w{6,}\z/
-  VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/
+  VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥々ー]+\z/
   VALID_NAME_KANA_REGEX = /\A[ァ-ヶー－]+\z/
 
   validates :nickname, presence: true
