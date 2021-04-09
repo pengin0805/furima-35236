a8256bd33efcbe8c157eefba9c6a8bf52151eba2
pengin0805 <79044858+pengin0805@users.noreply.github.com>
2021-04-09 17:02:05 +0900

    ログイン状態のユーザーのみ商品出品ページへ遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,12 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    if user_signed_in?
+      @items = Item.new
+    else
+      redirect_to new_user_session_path
+    end
+  end
 end
