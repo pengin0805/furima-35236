ba10c3273d65705b53a6ecf233d433adbf119b5e
pengin0805 <79044858+pengin0805@users.noreply.github.com>
2021-04-09 17:51:17 +0900

    form_with内にキー修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,9 +4,18 @@ class ItemsController < ApplicationController
 
   def new
     if user_signed_in?
-      @items = Item.new
+      @item = Item.new
     else
       redirect_to new_user_session_path
     end
+  end
+
+  def create
+    Item.create(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:item_name, :item_info, :category_id, :condition_id, :shipping_fee_id, :shipping_area_id, :shipping_day_id, :price).merge(user_id: current_user.id)
   end
 end
