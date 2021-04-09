83f33b0bf5539cc23ca53c88077871a7e1e3fb06
pengin0805 <79044858+pengin0805@users.noreply.github.com>
2021-04-09 11:42:53 +0900

    rubocopにて自動修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,16 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  VALID_PASSWORD_REGEX =/\A(?=.*?[a-z])(?=.*?[\d])\w{6,}\z/
-  VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/ 
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)\w{6,}\z/
+  VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/
   VALID_NAME_KANA_REGEX = /\A[ァ-ヶー－]+\z/
 
   validates :nickname, presence: true
-  validates :last_name, presence: true, format: { with: VALID_NAME_REGEX, message: "は全角（漢字・ひらがな・カタカナ）であること"}
-  validates :first_name, presence: true, format: { with: VALID_NAME_REGEX, message: "は全角（漢字・ひらがな・カタカナ）であること"}
-  validates :last_name_kana, presence: true, format: { with: VALID_NAME_KANA_REGEX, message: "は全角であること"}
-  validates :first_name_kana, presence: true, format: { with: VALID_NAME_KANA_REGEX, message: "は全角であること"}
+  validates :last_name, presence: true, format: { with: VALID_NAME_REGEX, message: 'は全角（漢字・ひらがな・カタカナ）であること' }
+  validates :first_name, presence: true, format: { with: VALID_NAME_REGEX, message: 'は全角（漢字・ひらがな・カタカナ）であること' }
+  validates :last_name_kana, presence: true, format: { with: VALID_NAME_KANA_REGEX, message: 'は全角であること' }
+  validates :first_name_kana, presence: true, format: { with: VALID_NAME_KANA_REGEX, message: 'は全角であること' }
   validates :birthday, presence: true
-  validates :password, format: { with: VALID_PASSWORD_REGEX, message: "は半角英数字混合であること"}
-
+  validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'は半角英数字混合であること' }
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,10 +3,10 @@ FactoryBot.define do
     transient do
       person { Gimei.name }
     end
-    nickname {Faker::Name.last_name}
-    email {Faker::Internet.free_email}
-    password  {Faker::Lorem.characters(min_alpha:1,min_numeric:1 ,number: 6)}
-    password_confirmation {password}
+    nickname { Faker::Name.last_name }
+    email { Faker::Internet.free_email }
+    password { Faker::Lorem.characters(min_alpha: 1, min_numeric: 1, number: 6) }
+    password_confirmation { password }
     last_name { person.last.kanji }
     first_name { person.first.kanji }
     last_name_kana { person.last.katakana }
