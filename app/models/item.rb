ccdfd7453123d057164db70698a1488e98e81aa7
pengin0805 <79044858+pengin0805@users.noreply.github.com>
2021-04-10 21:54:36 +0900

    Active Hashの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,14 +1,25 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_fee
+  belongs_to :shipping_area
+  belongs_to :shipping_day
   
   validates :item_name, presence: true
   validates :item_info, presence: true
-  validates :category_id, presence: true
-  validates :condition_id, presence: true
-  validates :shipping_fee_id, presence: true
-  validates :shipping_area_id, presence: true
-  validates :shipping_day_id, presence: true
+  validates :category_id, numericality: { other_than: 1 }
+  validates :condition_id, numericality: { other_than: 1 }
+  validates :shipping_fee_id, numericality: { other_than: 1 }
+  validates :shipping_area_id, numericality: { other_than: 1 }
+  validates :shipping_day_id, numericality: { other_than: 1 }
   validates :price, presence: true
-  
+  validates :image, presence: true, unless: :attached?
+
+  def attached?
+    self.image.attached?
+  end
+
 end
