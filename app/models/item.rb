a35b39ff2ee7773309a30b44ce6dac857ca0b12a
pengin0805 <79044858+pengin0805@users.noreply.github.com>
2021-04-11 18:23:41 +0900

    単体テストコードの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -15,8 +15,17 @@ class Item < ApplicationRecord
   validates :shipping_fee_id, numericality: { other_than: 1 }
   validates :shipping_area_id, numericality: { other_than: 1 }
   validates :shipping_day_id, numericality: { other_than: 1 }
-  validates :price, presence: true, numericality: {greater_than: 300,less_than: 9999999 },format: { with: /\A[0-9]+\z/, message: 'は半角であること' }
+  validates :price, format: { with: /\A[0-9]+\z/}
+  validates :price, numericality: {greater_than_or_equal_to: 300,less_than_or_equal_to: 9999999 }
   validates :image, presence: true, unless: :attached?
+
+  # def price_error
+  #   if price.blank?*
+  #     errors[:base] = 'd'
+  #   elsif email.match(/\A[0-9]+\z/) ==nil
+  #     errors[:base] = 'e'
+  #   end
+  # end
 
   def attached?
     self.image.attached?
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,117 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe 'itemの新規登録' do
+    context 'itemの新規登録ができるとき' do
+      it '全て入力があれば登録できること' do
+        expect(@item).to be_valid
+      end
+      it 'priceは¥300~¥9,999,999の間であれば保存できること' do
+        @item.price = '1234'
+        expect(@item).to be_valid
+      end
+      it 'priceは半角数字であれば保存できること' do
+        @item.price = '1111'
+        expect(@item).to be_valid
+      end
+    end
+
+    context 'itemの新規登録ができないとき' do
+      it 'imageが空では登録できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Image can't be blank"
+      end
+      it 'item_nameが空では登録できない' do
+        @item.item_name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Item name can't be blank"
+      end
+      it 'item_infoが空では登録できない' do
+        @item.item_info = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Item info can't be blank"
+      end
+      it 'category_idが空では登録できない' do
+        @item.category_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Category is not a number"
+      end
+      it 'category_idが1では登録できない' do
+        @item.category_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Category must be other than 1"
+      end
+      it 'condition_idが空では登録できない' do
+        @item.condition_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Condition is not a number"
+      end
+      it 'condition_idが1では登録できない' do
+        @item.condition_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Condition must be other than 1"
+      end
+      it 'shipping_fee_idが空では登録できない' do
+        @item.shipping_fee_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Shipping fee is not a number"
+      end
+      it 'shipping_fee_idが1では登録できない' do
+        @item.shipping_fee_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Shipping fee must be other than 1"
+      end
+      it 'shipping_area_idが空では登録できない' do
+        @item.shipping_area_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Shipping area is not a number"
+      end
+      it 'shipping_fee_idが1では登録できない' do
+        @item.shipping_area_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Shipping area must be other than 1"
+      end
+      it 'shipping_day_idが空では登録できない' do
+        @item.shipping_day_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Shipping day is not a number"
+      end
+      it 'shipping_fee_idが1では登録できない' do
+        @item.shipping_day_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Shipping day must be other than 1"
+      end
+      it 'priceが空では登録できない' do
+        @item.price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Price is not a number"
+      end
+      it 'priceが¥300より下だと登録できない' do
+        @item.price = '100'
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Price must be greater than or equal to 300"
+      end
+      it 'priceが¥9,999,999より上だと登録できない' do
+        @item.price = '10000000'
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Price must be less than or equal to 9999999"
+      end
+      it 'priceが全角だと登録できない' do
+        @item.price = '１１１１'
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Price is not a number"
+      end
+      it 'userが紐付いていないと登録できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("User must exist")
+      end
+    end
+  end
 end
+
