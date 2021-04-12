6962f81cf280266e6d73ec3650053c24f1e344af
pengin0805 <79044858+pengin0805@users.noreply.github.com>
2021-04-12 11:08:29 +0900

    バリデーション　コード短縮

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :condition
@@ -8,24 +9,19 @@ class Item < ApplicationRecord
   belongs_to :shipping_area
   belongs_to :shipping_day
   
-  validates :item_name, presence: true
-  validates :item_info, presence: true
-  validates :category_id, numericality: { other_than: 1 }
-  validates :condition_id, numericality: { other_than: 1 }
-  validates :shipping_fee_id, numericality: { other_than: 1 }
-  validates :shipping_area_id, numericality: { other_than: 1 }
-  validates :shipping_day_id, numericality: { other_than: 1 }
-  validates :price, format: { with: /\A[0-9]+\z/}
-  validates :price, numericality: {greater_than_or_equal_to: 300,less_than_or_equal_to: 9999999 }
+  with_options presence: true do
+    validates :item_name
+    validates :item_info
+  end
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :condition_id
+    validates :shipping_fee_id
+    validates :shipping_area_id
+    validates :shipping_day_id
+  end
+  validates :price, format: { with: /\A[0-9]+\z/, message: "は半角にしてください"}, numericality: {greater_than_or_equal_to: 300,less_than_or_equal_to: 9999999 }
   validates :image, presence: true, unless: :attached?
-
-  # def price_error
-  #   if price.blank?*
-  #     errors[:base] = 'd'
-  #   elsif email.match(/\A[0-9]+\z/) ==nil
-  #     errors[:base] = 'e'
-  #   end
-  # end
 
   def attached?
     self.image.attached?
