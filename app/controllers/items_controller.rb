03a4b0dc2748c7b9302265db4abe0e783d928408
pengin0805 <79044858+pengin0805@users.noreply.github.com>
2021-04-13 14:18:19 +0900

    編集ページへのアクセス制限

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
+  before_action :move_to_index, only: :edit
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -41,4 +42,11 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:item_name, :item_info, :category_id, :condition_id, :shipping_fee_id, :shipping_area_id,
                                  :shipping_day_id, :price, :image).merge(user_id: current_user.id)
   end
+
+  def move_to_index
+    unless user_signed_in? && current_user.id == Item.find(params[:id]).user_id
+      redirect_to root_path
+    end
+  end
+
 end
