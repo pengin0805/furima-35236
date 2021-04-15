f0e7e615889fed7fb1c040cf7f8c253f7eb8b73b
pengin0805 <79044858+pengin0805@users.noreply.github.com>
2021-04-15 21:57:28 +0900

    formオブジェクトパターンの作成

---
+++ app/models/order_address.rb
@@ -0,0 +1,19 @@
+class OrderAddress
+  include ActiveModel::Model
+  attr_accessor :item_id, :user_id, :postal_code, :metropolis_id, :city, :address, :building, :phone_number
+
+  with_options presence: true do
+    validates :item_id
+    validates :user_id
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :city
+    validates :address
+    validates :phone_number, format: {with: /\A\d{,11}\z/}
+  end
+  validates :metropolis_id, numericality: {other_than: 0, message: "can't be blank"}
+
+  def save
+    order = Order.create(item_id: item_id, user_id: user_id)
+    Address.create(postal_code: postal_code, city: city, building: building, address: address, phone_number: phone_number, metropolis_id: metropolis_id, order_id: order.id)
+  end
+end
