57bd3db5de01a068a8af4aeeb4d8c03b74171d4d
pengin0805 <79044858+pengin0805@users.noreply.github.com>
2021-04-16 09:41:07 +0900

    turbolinksに関する記述の削除

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,7 +9,7 @@ class OrdersController < ApplicationController
   def create
     @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new(order_params)
-    if @order_addrels ss.valid?
+    if @order_address.valid?
       @order_address.save
     redirect_to root_path
     else
