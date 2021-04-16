bffac4d6e1bae82b251a7ceffa1c8beb108d5593
pengin0805 <79044858+pengin0805@users.noreply.github.com>
2021-04-16 17:04:18 +0900

    tokenのテストコード追加

--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -55,6 +55,11 @@ RSpec.describe OrderAddress, type: :model do
         @order_address.valid?
         expect(@order_address.errors.full_messages).to include("Metropolis can't be blank")
       end
+      it 'tokenが空では保存できない' do
+        @order_address.token = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Token can't be blank")
+      end
     end
   end
 end
