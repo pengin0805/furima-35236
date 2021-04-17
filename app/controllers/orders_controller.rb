c4e191df8463aff6eb7f2e096e4dbd190a3dc353
pengin0805 <79044858+pengin0805@users.noreply.github.com>
2021-04-17 10:14:53 +0900

    リファクタリング

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,19 +1,18 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_item, only: [:index, :create]
   before_action :move_to_index, only: [:index, :create]
 
   def index
-    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
       pay_item
       @order_address.save
-    redirect_to root_path
+      redirect_to root_path
     else
       render :index
     end
@@ -35,9 +34,13 @@ class OrdersController < ApplicationController
   end
 
   def move_to_index
-    if current_user.id == Item.find(params[:item_id]).user_id
+    if current_user.id == @item.user_id || @item.order
       redirect_to root_path
     end
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
   end
 
 end
