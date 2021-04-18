3654eb944d51c7409b5c390e00e83f28fa6cfd6e
pengin0805 <79044858+pengin0805@users.noreply.github.com>
2021-04-18 11:52:40 +0900

    phone_numberのバリデーション修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -8,7 +8,7 @@ class OrderAddress
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :city
     validates :address
-    validates :phone_number, format: {with: /\A\d{,11}\z/}
+    validates :phone_number, format: {with: /\A\d{10,11}\z/}
     validates :token
   end
   validates :metropolis_id, numericality: {other_than: 1, message: "can't be blank"}
