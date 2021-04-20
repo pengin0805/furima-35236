0f9d3acddf3658f58a1af99de0e7e9c7ff85bfb3
pengin0805 <79044858+pengin0805@users.noreply.github.com>
2021-04-20 12:48:40 +0900

    capistranoのバーション修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.1"
+lock "3.16.0"
 
 set :application, "furima-35236"
 set :repo_url, "git@github.com:pengin0805/furima-35236.git"
